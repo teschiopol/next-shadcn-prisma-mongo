85315580d710b42857846ff97bca33269d996405
teschiopol <concolatop@gmail.com>
2024-07-12 02:13:20 +0200

    style: pretty base

--- src/app/columns.tsx
+++ src/app/columns.tsx
@@ -1,24 +1,85 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
 
-export type Status = {
-    status:  "new" | "pending" | "processing" | "success" | "failed"
-}
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export type Task = {
   id: string
   content: string
-  status: Status
+  status: "new" | "pending" | "processing" | "success" | "failed"
 }
 
 export const columns: ColumnDef<Task>[] = [
-    {
-        accessorKey: "content",
-        header: "Task",
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "content",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="p-0"
+        >
+          Task
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
     },
-    {
-        accessorKey: "status",
-        header: "Status",
-    }
+  },
+  {
+    accessorKey: "status",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          className="p-0"
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+     
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-green-700">Complete</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-700">Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  }
 ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { prisma } from "@/utils";
 import { Task, columns } from "./columns"
 import { DataTable } from "./data-table"
 import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
 
 async function getData(): Promise<Task[]> {
   // Fetch data from your API here.
   const allTask = await prisma.task.findMany()
+  // @ts-ignore
   return allTask
 }
 
@@ -13,7 +15,8 @@ async function addTask() {
   await prisma.task.create({
     data: {
       content: 'Added task',
-      status: 'new',
+      // @ts-ignore
+      status: 'new', 
     },
   })
 }
@@ -23,11 +26,13 @@ export default async function Home() {
 
   return (
     <main className="">
-      <div className="text-center my-5 flex justify-center">
-        <h1 className="text-2xl font-bold">ToDo List</h1>
+      <div className="text-center my-8 flex justify-center">
+        <h1 className="text-2xl font-semibold">ToDo List</h1>
       </div>
-      <div className="flex mx-10">
-        <Button>Add New</Button>
+      <div className="container mx-auto">
+        <Button>
+          <Plus className="mr-2 h-4 w-4" />Add New
+        </Button>
       </div>
       <div className="container mx-auto py-10">
         <DataTable columns={columns} data={data} />
